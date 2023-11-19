ba7a75822867acef80d1b95d0350befc09b4c417
Erick Awino <erick.otieno89@gmail.com>
2023-11-19 21:16:45 +0300

    Quick Sort

--- sort.h
+++ sort.h
@@ -23,7 +23,7 @@ typedef struct listint_s
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-
+void quick_sort(int *array, size_t size);
 /* Advanced Task Prototypes */
 
 
