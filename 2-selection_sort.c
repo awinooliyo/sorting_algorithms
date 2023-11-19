5fa5412584798a164b64a7fbadbc455f48519378
Erick Awino <erick.otieno89@gmail.com>
2023-11-19 15:12:58 +0300

    Selection  Sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -28,21 +28,25 @@ void swap_ints(int *a, int *b)
 */
 void selection_sort(int *array, size_t size)
 {
-	int *min;
-	size_t i, j;
+	size_t i, j, min_index;
 
-	if (!array || size < 2)
+	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		min = array + i;
+		min_index = i;
 		for (j = i + 1; j < size; j++)
-			min = (array[j] < *min) ? (array + j) : min;
-
-		if ((array + i) != min)
 		{
-			swap_ints(array + 1, min);
+			if (array[j] < array[min_index])
+			{
+				min_index = j;
+			}
+		}
+
+		if (min_index != i)
+		{
+			swap_ints(&array[min_index], &array[i]);
 			print_array(array, size);
 		}
 	}
