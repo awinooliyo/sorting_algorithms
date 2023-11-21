430f04e9abbc302d71422ae9176b559bc90267b7
Erick Awino <erick.otieno89@gmail.com>
2023-11-21 13:36:43 +0300

    Heap Sort

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,5 +1,8 @@
 #include "sort.h"
 
+void swap_ints(int *a, int *b);
+void max_heapify(int *array, size_t size, size_t base, size_t root);
+void heap_sort(int *array, size_t size);
 
 /**
  * swap_ints - Swap two integers in an array.
